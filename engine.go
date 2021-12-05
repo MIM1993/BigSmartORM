17de60f6063fe4710e66d0d2858ee7144b5a7ab0
muyiming <13511085358@163.com>
2021-12-05 14:39:57 +0800

    init

---
+++ engine.go
@@ -0,0 +1,12 @@
+/*
+@Time : 2021/12/5 下午2:33
+@Author : MuYiMing
+@File : engine
+@Software: GoLand
+*/
+package BigSmartORM
+
+//mysql Engine 创建核心引擎，最终要求实现Enginer接口
+type MysqlEngine struct {
+
+}
---
+++ mysqlConn.go
@@ -0,0 +1,11 @@
+/*
+@Time : 2021/12/5 下午2:36
+@Author : MuYiMing
+@File : mysqlConn
+@Software: GoLand
+*/
+package BigSmartORM
+
+func NewMysqlConn() {
+
+}
